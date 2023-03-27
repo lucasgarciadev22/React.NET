4a44fec99e513d16e0de05106ad9782c3250e5cc
Lucas Miguel Garcia da Silva <lucas10@weg.net>
2023-03-27 13:18:51 -0300

    Update Logs

--- PaymentSimulatorCloudApi/backend/Controllers/SellerController.cs
+++ PaymentSimulatorCloudApi/backend/Controllers/SellerController.cs
@@ -5,141 +5,160 @@ using tech_test_payment_api.Models.Helpers;
 
 namespace tech_test_payment_api.Controllers
 {
-  [ApiController]
-  [Produces("application/json")]
-  [Consumes("application/json")]
-  [Route("[controller]")]
-  public class SellerController : ControllerBase
-  {
-    private readonly OrderContext _context;
-
-    public SellerController(OrderContext context)
+    [ApiController]
+    [Produces("application/json")]
+    [Consumes("application/json")]
+    [Route("[controller]")]
+    public class SellerController : ControllerBase
     {
-      _context = context;
+        private readonly OrderContext _context;
+        private readonly string _storageAccConnection;
+        private readonly string _tableName;
+        private TableClient _tableClient;
+
+        public SellerController(OrderContext context)
+        {
+            _context = context;
+            _storageAccConnection = configuration.GetValue<string>("ConnectionStrings:StorageAccConnection");
+            _tableName = configuration.GetValue<string>("ConnectionStrings:AzureTableName");
+            _tableClient = ContextHelper.GetAzureTableClient(_tableName, _storageAccConnection);
+        }
+
+        /// <summary>
+        /// Registers a new seller in the database.
+        /// </summary>
+        /// <param name="SellerToRegister"></param>
+        /// <returns>A newly registered seller</returns>
+        /// <remarks>
+        /// Example of request (mandatory parameters):
+        /// Id -> auto-incremented by SQL.
+        ///
+        /// POST /Seller
+        /// {
+        /// "cpf": "555555888-88",
+        /// }
+        /// </remarks>
+        /// <response code="201">If the seller was successfully registered</response>
+        /// <response code="400">If the credentials are incorrect</response>
+        // // // /// <response code="404">If the seller is not found</response>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult RegisterSeller(Seller seller)
+        {
+            _context.Add(seller);
+            _context.SaveChanges();
+
+            SellerLog sellerLog = new SellerLog(seller, ActionType.Insert, seller.Cpf, Guid.NewGuid().ToString());
+            _tableClient.UpsertEntity(sellerLog);
+
+            return CreatedAtAction(nameof(GetSellerById), new { id = seller.Id }, seller);
+        }
+
+        /// <summary>
+        /// Searches for a seller based on the provided Id.
+        /// </summary>
+        /// <param name="SellerToFind"></param>
+        /// <returns></returns>
+        /// <remarks>
+        /// Example of request (mandatory parameters):
+        ///
+        /// GET /Seller
+        /// {
+        /// "id": 1,
+        /// }
+        /// </remarks>
+        /// <response code="200">If the request returns a seller</response>
+        /// <response code="404">If the seller is not found</response>
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult GetSellerById(int id)
+        {
+            var seller = _context.Sellers.Find(id);
+            if (seller == null)
+            {
+                return NotFound(StatusMessage.ShowErrorMessage(ClassType.Seller, $"{id}", ContextHelper.GetCurrentCatalog(_context), ActionType.Get));
+            }
+            return Ok(seller);
+        }
+
+        /// <summary>
+        /// Updates the information of a previously registered seller.
+        /// </summary>
+        /// <param name="SellerToUpdate"></param>
+        /// <returns>Updated seller information</returns>
+        /// <remarks>
+        /// Example of request (required parameters):
+        ///
+        /// PUT /Seller
+        /// {
+        /// "id": 1,
+        /// }
+        /// </remarks>
+        /// <response code="200">If the request successfully updates the seller</response>
+        /// <response code="404">If the seller is not found</response>
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult EditSellerInfo(int id, Seller seller)
+        {
+            var sellerToUpdate = _context.Sellers.Find(id);
+
+            if (sellerToUpdate == null)
+            {
+                return NotFound(StatusMessage.ShowErrorMessage(ClassType.Seller, $"{id}", ContextHelper.GetCurrentCatalog(_context), ActionType.Update));
+            }
+
+            sellerToUpdate.Cpf = seller.Cpf;
+            sellerToUpdate.Name = seller.Name;
+            sellerToUpdate.Email = seller.Email;
+            sellerToUpdate.Phone = seller.Phone;
+
+            _context.Sellers.Update(sellerToUpdate);
+            _context.SaveChanges();
+
+            SellerLog sellerLog = new SellerLog(seller, ActionType.Update, seller.Cpf, Guid.NewGuid().ToString());
+            _tableClient.UpsertEntity(sellerLog);
+
+            return Ok(StatusMessage.ShowConfirmationMessage(ClassType.Seller, $"{sellerToUpdate.Id}", ContextHelper.GetCurrentCatalog(_context), ActionType.Update));
+        }
+
+        /// <summary>
+        /// Removes a seller from the database according to the provided Id.
+        /// </summary>
+        /// <param nameof="SellerToDelete"></param>
+        /// <returns>Informs which seller has been removed</returns>
+        /// <remarks>
+        ///   Example request (required parameters):         
+        ///
+        ///     PUT /Seller
+        ///     {
+        ///        "id": 1,
+        ///     }
+        /// </remarks>
+        /// <response code="200">If the request successfully removes the seller</response>
+        /// <response code="404">If the seller is not found</response>
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult DeleteSellerInfo(int id, Seller seller)
+        {
+            var sellerToDelete = _context.Sellers.Find(id);
+
+            if (sellerToDelete == null)
+            {
+                return NotFound(StatusMessage.ShowErrorMessage(ClassType.Seller, $"{id}", ContextHelper.GetCurrentCatalog(_context), ActionType.Get));
+            }
+
+            _context.Sellers.Remove(sellerToDelete);
+            _context.SaveChanges();
+
+            SellerLog sellerLog = new SellerLog(seller, ActionType.Remove, seller.Cpf, Guid.NewGuid().ToString());
+            _tableClient.UpsertEntity(sellerLog);
+
+            return Ok(StatusMessage.ShowConfirmationMessage(ClassType.Seller, $"{sellerToDelete.Id}", ContextHelper.GetCurrentCatalog(_context), ActionType.Remove));
+        }
     }
-    /// <summary>
-    /// Registers a new seller in the database.
-    /// </summary>
-    /// <param name="SellerToRegister"></param>
-    /// <returns>A newly registered seller</returns>
-    /// <remarks>
-    /// Example of request (mandatory parameters):
-    /// Id -> auto-incremented by SQL.
-    ///
-    /// POST /Seller
-    /// {
-    /// "cpf": "555555888-88",
-    /// }
-    /// </remarks>
-    /// <response code="201">If the seller was successfully registered</response>
-    /// <response code="400">If the credentials are incorrect</response>
-    // // // /// <response code="404">If the seller is not found</response>
-    [HttpPost]
-    [ProducesResponseType(StatusCodes.Status201Created)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public IActionResult RegisterSeller(Seller seller)
-    {
-      _context.Add(seller);
-      _context.SaveChanges();
-
-      return CreatedAtAction(nameof(GetSellerById), new { id = seller.Id }, seller);
-    }
-    /// <summary>
-    /// Searches for a seller based on the provided Id.
-    /// </summary>
-    /// <param name="SellerToFind"></param>
-    /// <returns></returns>
-    /// <remarks>
-    /// Example of request (mandatory parameters):
-    ///
-    /// GET /Seller
-    /// {
-    /// "id": 1,
-    /// }
-    /// </remarks>
-    /// <response code="200">If the request returns a seller</response>
-    /// <response code="404">If the seller is not found</response>
-    [HttpGet("{id}")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public IActionResult GetSellerById(int id)
-    {
-      var seller = _context.Sellers.Find(id);
-      if (seller == null)
-      {
-        return NotFound(StatusMessage.ShowErrorMessage(ClassType.Seller, $"{id}", ContextHelper.GetCurrentCatalog(_context), ActionType.Get));
-      }
-      return Ok(seller);
-    }
-    /// <summary>
-    /// Updates the information of a previously registered seller.
-    /// </summary>
-    /// <param name="SellerToUpdate"></param>
-    /// <returns>Updated seller information</returns>
-    /// <remarks>
-    /// Example of request (required parameters):
-    ///
-    /// PUT /Seller
-    /// {
-    /// "id": 1,
-    /// }
-    /// </remarks>
-    /// <response code="200">If the request successfully updates the seller</response>
-    /// <response code="404">If the seller is not found</response>
-    [HttpPut("{id}")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public IActionResult EditSellerInfo(int id, Seller seller)
-    {
-      var sellerToUpdate = _context.Sellers.Find(id);
-
-      if (sellerToUpdate == null)
-      {
-        return NotFound(StatusMessage.ShowErrorMessage(ClassType.Seller, $"{id}", ContextHelper.GetCurrentCatalog(_context), ActionType.Update));
-      }
-
-      sellerToUpdate.Cpf = seller.Cpf;
-      sellerToUpdate.Name = seller.Name;
-      sellerToUpdate.Email = seller.Email;
-      sellerToUpdate.Phone = seller.Phone;
-
-      _context.Sellers.Update(sellerToUpdate);
-      _context.SaveChanges();
-
-      return Ok(StatusMessage.ShowConfirmationMessage(ClassType.Seller, $"{sellerToUpdate.Id}", ContextHelper.GetCurrentCatalog(_context), ActionType.Update));
-    }
-    /// <summary>
-    /// Removes a seller from the database according to the provided Id.
-    /// </summary>
-    /// <param nameof="SellerToDelete"></param>
-    /// <returns>Informs which seller has been removed</returns>
-    /// <remarks>
-    ///   Example request (required parameters):         
-    ///
-    ///     PUT /Seller
-    ///     {
-    ///        "id": 1,
-    ///     }
-    /// </remarks>
-    /// <response code="200">If the request successfully removes the seller</response>
-    /// <response code="404">If the seller is not found</response>
-    [HttpDelete("{id}")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public IActionResult DeleteSellerInfo(int id, Seller seller)
-    {
-      var sellerToDelete = _context.Sellers.Find(id);
-
-      if (sellerToDelete == null)
-      {
-        return NotFound(StatusMessage.ShowErrorMessage(ClassType.Seller, $"{id}", ContextHelper.GetCurrentCatalog(_context), ActionType.Get));
-      }
-
-      _context.Sellers.Remove(sellerToDelete);
-      _context.SaveChanges();
-
-      return Ok(StatusMessage.ShowConfirmationMessage(ClassType.Seller, $"{sellerToDelete.Id}", ContextHelper.GetCurrentCatalog(_context), ActionType.Remove));
-    }
-  }
 }
--- PaymentSimulatorCloudApi/backend/Models/OrderRegistryLog.cs
+++ PaymentSimulatorCloudApi/backend/Models/OrderRegistryLog.cs
@@ -6,17 +6,6 @@ namespace tech_test_payment_api.Models
 {
   public class OrderRegistryLog : OrderRegistry, ITableEntity
   {
-    public OrderRegistryLog() { }
-    public OrderRegistryLog(OrderRegistry orderRegistry, ActionType actionType,string partitionKey, string rowKey)
-    { 
-      base.Id = orderRegistry.Id;
-      base.OrderDate = orderRegistry.OrderDate;
-      base.OrderNumber = orderRegistry.OrderNumber;
-      ActionType = actionType;
-      OrderRegistryJson = JsonSerializer.Serialize(orderRegistry);
-      PartitionKey = partitionKey;
-      RowKey= rowKey;
-    }
     public ActionType ActionType { get; set; }
     public string OrderRegistryJson { get; set; }
     //Azure Table Properties...
@@ -24,5 +13,16 @@ namespace tech_test_payment_api.Models
     public string RowKey { get; set; }
     public DateTimeOffset? Timestamp { get; set; }
     public ETag ETag { get; set; }
+    public OrderRegistryLog() { }
+    public OrderRegistryLog(OrderRegistry orderRegistry, ActionType actionType,string partitionKey, string rowKey)
+    { 
+      base.Id = orderRegistry.Id;
+      base.OrderDate = orderRegistry.OrderDate;
+      base.SellerCpf = orderRegistry.SellerCpf;
+      ActionType = actionType;
+      OrderRegistryJson = JsonSerializer.Serialize(orderRegistry);
+      PartitionKey = partitionKey;
+      RowKey= rowKey;
+    }
   }
 }
